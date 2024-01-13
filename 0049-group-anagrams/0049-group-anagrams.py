4f3fad836206f7dc584466ffe3a1ce986c7483ae
Divya Thomas <66500398+divyasthomas@users.noreply.github.com>
2024-01-13 00:54:39 -0600

    Time: 93 ms (71.71%), Space: 23.3 MB (8.23%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,89 +1,105 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        #O(nlogN*m solution)
-        
-        string_dict={}
-        
+
+    #O(m*n*26) solution
+        anagrams=defaultdict(list)
+    
+    
+         #make a dict
         for idx in range(len(strs)):
-            s = tuple(sorted(strs[idx]))
-            if string_dict.get(s,0):
-                string_dict[s].append(strs[idx])
-                # print("true", string_dict[s])
+            arr=[0]*26
+
+            for c in strs[idx]:
+                arr[ord(c)-ord('a')]+=1
+            
+            
+            anagrams[tuple(arr)].append(strs[idx])
+
+        
+        return anagrams.values()
+            
+            
+            
+            
+
+    
+    
+    
+    
                 
-            else:
-                string_dict[s]=[strs[idx]]
-                # print("false", string_dict[s])
         
-        return string_dict.values()
+
+
+        
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        #O(nlogN*m solution)
+
+#         string_dict={}
+        
+#         for idx in range(len(strs)):
+#             s = tuple(sorted(strs[idx]))
+#             if string_dict.get(s,0):
+#                 string_dict[s].append(strs[idx])
+#                 # print("true", string_dict[s])
                 
-        
+#             else:
+#                 string_dict[s]=[strs[idx]]
+#                 # print("false", string_dict[s])  
+#         return string_dict.values()
+    
+    
+    
+    
+    
+    
 
-
-        
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-            #simplest - nlog(n) *m
-        # my_dict={}
-        # for i in range(len(strs)):
-        #     sorted_word = ''.join(sorted(strs[i]))
-        #     if my_dict.get(sorted_word,0) == 0:
-        #         my_dict[sorted_word] = [strs[i]]
-        #     else:
-        #         my_dict[sorted_word].append(strs[i])
-        # return my_dict.values()
-
-  #O(m*n*26) solution
+#   #O(m*n*26) solution
 
 #         result=defaultdict(list)
 
@@ -96,6 +112,7 @@ class Solution:
 #             #create that list for each string in strs
 #             for ch in s:
 #                 count[ord(ch)-ord('a')] += 1
+                
 #             #make a hash where you use that list(tuple) as key and store string as value - same as before
 
 #             result[tuple(count)].append(s)
